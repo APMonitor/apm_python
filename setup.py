adc447672bd0849c199a686df9fefa5fa9d93f5b
okesontj <okesontj@users.noreply.github.com>
2018-01-31 11:20:51 -0700

    Added the setup file for uploading to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'APMonitor',
+  packages = ['APMonitor'], # this must be the same as the name above
+  version = '0.31',
+  description = 'Python package to interface with the APMonitor optimization suite',
+  author = 'John Hedengren',
+  author_email = 'john_hedengren@byu.edu',
+  url = 'https://github.com/okesontj/APMonitor', # use the URL to the github repo
+  download_url = 'https://github.com/okesontj/APMonitor/tarball/0.2',
+  keywords = ['optimization', 'MPC'], 
+  classifiers = [],
+)
